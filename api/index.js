108af9506853215976a4a8a6572c0ab77558e761
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 22:57:58 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=8XZXK1FIUA3&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=3NAS17RJ28O&afp=tapib' });
     res.end();
   }
