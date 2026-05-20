4359f681cbfe8637a882c64677811c7298fab732
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-19 21:52:30 -0300

    chore: update shareCode to ATN7IPE84XZ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=5033880EJTK&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=ATN7IPE84XZ&afp=tapib' });
     res.end();
   }
